2f1717f6df23b0f306915a67882d133991c54deb
obasemola <obasemolaayodele@gmail.com>
2020-07-19 15:35:37 +0300

    display both total tip and total bill

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const getServiceQual = document.querySelector('#options');
 
 // console.log(getServiceQual[2].value);
 
-const displayBill = (billTotal) => {
+const displayBill = (tipTotal, total) => {
   const text = `
-  Your bill is $${billTotal}!
+  Your tip is $${tipTotal} and your total bill is $${total}!
   `;
   display.textContent = text;
 
@@ -19,36 +19,44 @@ const displayBill = (billTotal) => {
 
 const calculateBill = () => {
   let tipTotal;
+  let total;
   submitBill.addEventListener('submit', (e) => {
     e.preventDefault();
-    const billValue = parseInt(submitBill.bill.value, 10);
-    const peopleValue = parseInt(submitBill.people.value, 10);
+    const billValue = parseFloat(submitBill.bill.value, 10);
+    const peopleValue = parseFloat(submitBill.people.value, 10);
     const selectedServiceQual = getServiceQual.selectedIndex;
     const serviceQualOptions = getServiceQual.options;
     const serviceQualIndex = serviceQualOptions[selectedServiceQual].index;
     const serviceQual = parseFloat(getServiceQual[serviceQualIndex].value);
 
-    if( billValue && !peopleValue || peopleValue == 0
+    if (!billValue) {
+      alert('Please type in your bill total');
+    }
+
+    if( billValue && !peopleValue || peopleValue == 0 || !serviceQual
 
       ) {
+      total = billValue;
+    }
+
+    if ( billValue && serviceQual && !peopleValue) {
       billTotal = billValue;
       tipTotal = parseFloat(billTotal * serviceQual);
       total = billTotal + tipTotal;
     }
 
-    if( billValue && peopleValue > 0) {
+    if( billValue && peopleValue > 0 && serviceQual) {
       billTotal = billValue / peopleValue;
       tipTotal = parseFloat(billTotal * serviceQual);
       total = billTotal + tipTotal;
       }
 
-      tipTotal = Math.round(billValue * serviceQual);
+      tipTotal = Math.round(tipTotal * 100) / 100;
       tipTotal = tipTotal.toFixed(2);
+      total = Math.round(total * 100) / 100;
+      total = total.toFixed(2);
 
-      console.log(serviceQual);
-      console.log(tipTotal);
-      console.log(total);
-      // displayBill(total);
+      displayBill(tipTotal, total);
   });
 }
 
