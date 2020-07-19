85a1bd8d0c110106caea8a612cd064697da80092
obasemola <obasemolaayodele@gmail.com>
2020-07-19 14:54:45 +0300

    added logic to calculate tip total

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const submitBill = document.querySelector('#billForm');
 const display = document.querySelector('.display');
 const container = document.querySelector('#container');
 const heading = document.querySelector('.h1');
+const getServiceQual = document.querySelector('#options');
+
+// console.log(getServiceQual[2].value);
 
 const displayBill = (billTotal) => {
   const text = `
@@ -15,24 +18,37 @@ const displayBill = (billTotal) => {
 };
 
 const calculateBill = () => {
-  let billTotal;
+  let tipTotal;
   submitBill.addEventListener('submit', (e) => {
     e.preventDefault();
     const billValue = parseInt(submitBill.bill.value, 10);
     const peopleValue = parseInt(submitBill.people.value, 10);
+    const selectedServiceQual = getServiceQual.selectedIndex;
+    const serviceQualOptions = getServiceQual.options;
+    const serviceQualIndex = serviceQualOptions[selectedServiceQual].index;
+    const serviceQual = parseFloat(getServiceQual[serviceQualIndex].value);
 
     if( billValue && !peopleValue || peopleValue == 0
 
       ) {
       billTotal = billValue;
-      // displayBill(billTotal)
+      tipTotal = parseFloat(billTotal * serviceQual);
+      total = billTotal + tipTotal;
     }
 
     if( billValue && peopleValue > 0) {
-      billTotal = billValue/peopleValue;
-        // displayBill(billTotal);
+      billTotal = billValue / peopleValue;
+      tipTotal = parseFloat(billTotal * serviceQual);
+      total = billTotal + tipTotal;
       }
-      displayBill(billTotal);
+
+      tipTotal = Math.round(billValue * serviceQual);
+      tipTotal = tipTotal.toFixed(2);
+
+      console.log(serviceQual);
+      console.log(tipTotal);
+      console.log(total);
+      // displayBill(total);
   });
 }
 
